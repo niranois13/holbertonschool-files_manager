db3a4eedcee1e6b5714c0f5ca066102e22df7214
niranois13 <8784@holbertonstudents.com>
2025-04-03 15:25:19 +0200

    Task 2 - fix linting

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,34 +5,34 @@ module.exports = {
   getStatus(req, res) {
     if (redisUtil.isAlive()) {
       if (dbUtil.isAlive()) {
-        res.status(200).json({ "redis": true, "db": true})
+        res.status(200).json({ redis: true, db: true });
       } else {
-        res.status(200).json({ "redis": true, "db": false})
+        res.status(200).json({ redis: true, db: false });
       }
     } else {
       if (dbUtil.isAlive()) {
-        res.status(200).json({ "redis": false, "db": true})
+        res.status(200).json({ redis: false, db: true });
       }
-      res.status(500).json({ "redis": false, "db": false})
+      res.status(500).json({ redis: false, db: false });
     }
   },
 
   async getStats(req, res) {
     try {
-      let dbUsers = await dbUtil.nbUsers(req);
-      let dbFiles = await dbUtil.nbFiles(req);
+      const dbUsers = await dbUtil.nbUsers(req);
+      const dbFiles = await dbUtil.nbFiles(req);
 
-      console.log("Users:", dbUsers);
-      console.log("Files:", dbFiles);
+      console.log('Users:', dbUsers);
+      console.log('Files:', dbFiles);
 
       if (dbUsers !== null && dbFiles !== null) {
-        res.status(200).json({ "users": dbUsers, "files": dbFiles});
+        res.status(200).json({ users: dbUsers, files: dbFiles });
       } else {
-        res.status(500).json({ "Error": "DB returned null values"});
+        res.status(500).json({ Error: 'DB returned null values' });
       }
     } catch (error) {
-      console.error("Error in getStats:", error);
-      res.status(500).json({ "Error": "Server error"} );
+      console.error('Error in getStats:', error);
+      res.status(500).json({ Error: 'Server error' });
     }
-  }
-}
+  },
+};
